c56b88f94b11fada9abdd5bcec32179d74b0ccfd
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-01-09 22:53:32 -0500

    debug

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,18 +1,15 @@
 <template>
-    <div class="about">
-      <div class="box">
-          <figure class="image">
-              <img src="../assets/TCoffeyResume.pdf">
-          </figure>
-        </div>
+  <div class="about">
+    <div class="box">
+      <figure class="image">
+        <img src="../assets/TCoffeyResume.pdf" />
+      </figure>
     </div>
-  </template>
-  
-  <style>
-  
-  .about {
-    background-color: #f5f5f5;
-   
-  }
-  </style>
-  
+  </div>
+</template>
+
+<style>
+.about {
+  background-color: #f5f5f5;
+}
+</style>
