48ede7ae4cb7319970ae46c4df8afd3c9ec3ef76
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-01-09 22:57:44 -0500

    improved footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,32 +1,43 @@
-<script setup lang= "ts">
-
+<script setup lang="ts">
 const isMenuActive = false;
-
 </script>
 
 <template>
-
-<footer class="footer">
-        <div class="content has-text-centered">
-          <p>
-             Thomas Coffey. Email: tomcoffey1017@gmail.com / coffeyt1@newpaltz.edu
-          </p>
-        </div>
-      </footer>
-
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <span class="footer-name">Thomas Coffey</span>
+        <br />
+        <span class="footer-email">
+          Email:
+          <a href="mailto:tomcoffey1017@gmail.com">tomcoffey1017@gmail.com</a> /
+          <a href="mailto:coffeyt1@newpaltz.edu">coffeyt1@newpaltz.edu</a>
+        </span>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+.footer {
+  background-color: #3498db;
+  color: white;
+  padding: 3rem;
+  margin-top: 1rem;
+}
 
-.footer{
-    background-color: lightgrey;
-    color: black;
-    padding: 3rem;
-    margin-top: 1rem;
-    margin-right: -15%;
-    margin-left: -15%;
-    margin-bottom: -1rem;
+.footer-name {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
 
-    
+.footer-email a {
+  color: white;
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+}
+
+.footer-email a:hover {
+  color: #ecf0f1;
 }
 </style>
